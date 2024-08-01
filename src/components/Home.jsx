71a7b74b4ec2ef7949cfa9e6fffe418f28f25ba3
Dhruv Sovasaria <dhruvsovasaria34@gmail.com>
2024-08-01 09:42:30 +0530

    fix animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,50 @@
 import React from "react";
-import { motion } from "framer-motion";
+import {
+  motion,
+  useInView,
+  useScroll,
+  useTransform,
+  useMotionValueEvent,
+} from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="flex w-full h-screen pl-10 pt-0">
-      <section
-        id="home"
-        className="home-section bg-codeblack text-white flex-[4] flex flex-col justify-start p-6 pt-20"
-      >
-        <h1 className="text-codelightlime home-text text-7xl pl-1 font-semibold">
-          Hi, I'm
-        </h1>
-        <h1 className=" text-codelime text-9xl font-semibold pb-2">
-          Dhruv Sovasaria
-        </h1>
-        <p className="home-text pl-2 text-5xl pt-14 font-light text-textcolor">
-          Web Developer
-        </p>
-        <a
-          href="https://www.linkedin.com/in/dhruvsovasaria/"
-          target="_blank"
-          className=" max-w-fit border-2 text-textcolor border-codelightlime my-10 text-sm rounded ml-3 hover:bg-buttonhovercolor font-medium px-4 py-3 hover:text-codelightlime transition-colors duration-150"
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ margin: "-400px 0px" }}
+    >
+      <div className="flex w-full h-screen pl-10 pt-0">
+        <section
+          id="home"
+          className="home-section bg-codeblack text-white flex-[4] flex flex-col justify-start p-6 pt-20"
         >
-          Checkout Linkedin
-        </a>
-      </section>
-      <section className="flex-[1] flex justify-center h-100 p-0 pr-5 items-end ">
-        <img
-          src="src/assets/dhruv-photo.png"
-          alt="dhruv-profile-photo"
-          className="   min-h-[80vh] object-cover"
-        />
-      </section>
-    </div>
+          <h1 className="text-codelightlime home-text text-7xl pl-1 font-semibold">
+            Hi, I'm
+          </h1>
+          <h1 className=" text-codelime text-9xl font-semibold pb-2">
+            Dhruv Sovasaria
+          </h1>
+          <p className="home-text pl-2 text-5xl pt-14 font-light text-textcolor">
+            Web Developer
+          </p>
+          <a
+            href="https://www.linkedin.com/in/dhruvsovasaria/"
+            target="_blank"
+            className=" max-w-fit border-2 text-textcolor border-codelightlime my-10 text-sm rounded ml-3 hover:bg-buttonhovercolor font-medium px-4 py-3 hover:text-codelightlime transition-colors duration-150"
+          >
+            Checkout Linkedin
+          </a>
+        </section>
+        <section className="flex-[1] flex justify-center h-100 p-0 pr-5 items-end ">
+          <img
+            src="src/assets/dhruv-photo.png"
+            alt="dhruv-profile-photo"
+            className="   min-h-[80vh] object-cover"
+          />
+        </section>
+      </div>
+    </motion.div>
   );
 };
 
