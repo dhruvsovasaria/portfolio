38b8e28a171f925301cd52cd008e729636bc4845
Dhruv Sovasaria <dhruvsovasaria34@gmail.com>
2024-08-12 19:53:01 +0530

    refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import dhruvPhoto from "src/assets/dhruv-photo.png"; // Import the image
 
 const Home = () => {
   return (
@@ -16,7 +17,7 @@ const Home = () => {
           <h1 className="text-codelightlime home-text text-7xl pl-1 font-semibold">
             Hi, I'm
           </h1>
-          <h1 className=" text-codelime text-9xl font-semibold pb-2">
+          <h1 className="text-codelime text-9xl font-semibold pb-2">
             Dhruv Sovasaria
           </h1>
           <p className="home-text pl-2 text-5xl pt-14 font-light text-textcolor">
@@ -25,16 +26,16 @@ const Home = () => {
           <a
             href="https://www.linkedin.com/in/dhruvsovasaria/"
             target="_blank"
-            className=" max-w-fit border-2 text-textcolor border-codelightlime my-10 text-sm rounded ml-3 hover:bg-buttonhovercolor font-medium px-4 py-3 hover:text-codelightlime transition-colors duration-150"
+            className="max-w-fit border-2 text-textcolor border-codelightlime my-10 text-sm rounded ml-3 hover:bg-buttonhovercolor font-medium px-4 py-3 hover:text-codelightlime transition-colors duration-150"
           >
             Checkout Linkedin
           </a>
         </section>
-        <section className="flex-[1] flex justify-center h-100 p-0 pr-5 items-end ">
+        <section className="flex-[1] flex justify-center h-100 p-0 pr-5 items-end">
           <img
-            src="src/assets/dhruv-photo.png"
+            src={dhruvPhoto} // Use the imported image here
             alt="dhruv-profile-photo"
-            className="   min-h-[80vh] object-cover"
+            className="min-h-[80vh] object-cover"
           />
         </section>
       </div>
